a942e50087a07863aa744cca1061804515c124f4
akuraya <ak.kuraya@gmail.com>
2021-12-21 23:08:25 +0900

    [#2]fmt

--- main.tf
+++ main.tf
@@ -1,12 +1,12 @@
 provider "google" {
-  credentials = "${file(var.credential.data)}"
-  project     = "${lookup(var.project_name, "${terraform.workspace}")}"
+  credentials = file(var.credential.data)
+  project     = lookup(var.project_name, "${terraform.workspace}")
   region      = "asia-northeast1"
 }
 
 resource "google_storage_bucket" "slack_functions_bucket" {
   name          = "${lookup(var.project_name, "${terraform.workspace}")}-scheduler-bucket"
-  project       = "${lookup(var.project_name, "${terraform.workspace}")}"
+  project       = lookup(var.project_name, "${terraform.workspace}")
   location      = "asia"
   force_destroy = true
 }
